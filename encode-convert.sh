a77fc0d00743adbcc1a4ec4f3a5360db0c166659
pottier <51529681+gylles38@users.noreply.github.com>
2023-04-28 15:48:48 +0200

    Update encode-convert.sh

--- encode-convert.sh
+++ encode-convert.sh
@@ -5,6 +5,6 @@ for entry in "$search_dir"/*
 do
     # converti l'encodage des fichiers 
     iconv $entry -f 'ISO-8859-1' -t 'UTF-8' -o $entry
-    # copie le fichier converti du pod sur le poste local
-		echo kubectl cp sse-ad-deployment-85d49dcbf-8z55x:$entry ${entry##/*/}		
+    # affiche l'instruction de copie du fichier converti du pod sur le poste local
+    echo kubectl cp sse-ad-deployment-85d49dcbf-8z55x:$entry ${entry##/*/}		
 done
